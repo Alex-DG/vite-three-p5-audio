f29c7f06abeff36d76d99a57853a6731c7dd1e70
Alex Di Guida <alexandre.diguida@gmail.com>
2022-07-17 17:28:02 +0100

    Tidy up css and dom elements

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
 import '../styles/app.css'
+import '../styles/credits.css'
+import '../styles/loading.css'
+import '../styles/player.css'
+
+import { Loading, AudioPlayer, Credits, Video } from './Dom'
 
 import Experience from './Experience'
 
 console.log('🎉', 'Project generated using vite-three-starter')
 console.log(':: https://github.com/Alex-DG/vite-three-starter ::')
+
+/**
+ * Dom
+ */
+document.querySelector('#app').innerHTML = `
+    ${Loading}
+    ${AudioPlayer}
+    ${Credits}
+    ${Video}
+`
 
 /**
  * Experience
